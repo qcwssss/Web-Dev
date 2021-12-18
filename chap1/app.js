c2ec3629c72c4cc2ddc33af3325e86842d4a4398
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-18 14:14:44 -0800

    first init

---
+++ chap1/app.js
@@ -0,0 +1,13 @@
+function hello(name) {
+    console.log('Hello ' + name);
+}
+
+hello('Mosh');
+
+// node doesn't have window or document obejct
+// console.log(window);
+
+// global
+
+// module
+// console.log(module);
