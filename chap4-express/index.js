8467283a1769fb60dfa7af1d381ef4bb4123609c
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-19 11:54:56 -0800

    express route

---
+++ chap4-express/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World, Brandon');
+}); 
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    // res.send(req.params);
+    res.send(req.query);
+
+
+})
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
