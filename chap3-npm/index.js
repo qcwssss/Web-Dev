a19e7396507eecd4d7b6f1e981711f471e3f91c1
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-19 09:54:41 -0800

    npm init

---
+++ chap3-npm/index.js
@@ -0,0 +1,9 @@
+const _ = require('underscore');
+
+// require() look for procedure
+// Core module
+// File or folder
+// node_modules
+
+const result = _.contains([1, 2, 3], 3);
+console.log(result);
