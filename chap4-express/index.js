4951d5283e3cb9a251e8810fe0a2f7bd9447abde
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-20 11:49:27 -0800

    refactor code

--- chap4-express/index.js
+++ chap4-express/index.js
@@ -20,7 +20,7 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID not found');
+    if (!course) return res.status(404).send('The course with the given ID not found');
     res.send(course);
 
 });
@@ -29,15 +29,12 @@ app.put('/api/courses/:id', (req, res) => {
     // Look up the course
     // If not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID not found');
+    if (!course) return res.status(404).send('The course with the given ID not found');
 
     // Validate
     // If invalid, return 400 - Bad request
     const { error } = validateCourse(req.body); // result.error
-    if (error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(result.error.details[0].message);
 
     // Update course
     course.course = req.body.course;
@@ -56,10 +53,7 @@ function validateCourse(course) {
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body); // result.error
-    if (error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(result.error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -68,6 +62,21 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID not found');
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the same course
+    res.send(course);
+})
 
 // PORT
 const port = process.env.PORT || 3000;
