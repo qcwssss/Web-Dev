c90def6a39f3ede9b106760d26665a9180c3fd4a
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-20 15:33:58 -0800

    add third-party midware

--- chap4-express/index.js
+++ chap4-express/index.js
@@ -1,12 +1,18 @@
+const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json()); // req.body -> json
 app.use(express.urlencoded({ extended: true })); // key=value && key=value
+app.use(express.static('public'));
 
 app.use(logger);
+
+app.use(helmet());
+app.use(morgan('tiny'));
 
 
 const courses = [
