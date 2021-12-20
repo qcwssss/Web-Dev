8e846feee0e4036c26df588540496c307f8a5d97
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-19 21:36:09 -0800

    add post

--- chap4-express/index.js
+++ chap4-express/index.js
@@ -1,19 +1,42 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, course: 'math1'},
+    {id: 2, course: 'math2'},
+    {id: 3, course: 'math3'}
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World, Brandon');
 }); 
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    // res.send(req.params);
-    res.send(req.query);
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    console.log(course);
+    if (!course) res.status(404).send('The course with the given ID not found');
+    res.send(course);
 
+});
 
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        course: Joi.string().min(3).required()
+    };
+
+    const course = {
+        id: courses.length + 1,
+        course: req.body.course
+    };
+    courses.push(course);
+    res.send(course);
 })
 
 // PORT
