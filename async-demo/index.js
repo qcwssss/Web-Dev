4584b64fa4dba1e4177b66750e433907a8a06f25
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-21 20:51:58 -0800

    add 2 nested callback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,13 @@
 console.log('Before');
 getUser(1, (user) => {
-    console.log('User', user);
+    // console.log('User', user);
+    // Get the repos
+    getRepositories(user.username, (userInfo) => {
+        console.log('Repos', userInfo.repos);        
+    })
+
 });
+
 console.log('After');
 
 // Callbacks
@@ -11,7 +17,12 @@ console.log('After');
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database...');
-        callback({id: id, userName: 'mosh'});
+        callback({id: id, username: 'mosh'});
     }, 2000);
+}
 
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback({username: username, repos: ['repo1', 'repo2', 'repo3']})
+    }, 2000);
 }
