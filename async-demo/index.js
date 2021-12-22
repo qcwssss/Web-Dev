c7c5d8c4579dc2d19c494af48606a157d65dd1ca
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-21 20:34:19 -0800

    setTimeout -> schedule

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,18 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000)
+const user = getUser(1);
+console.log(user);
 console.log('After');
+
+// Callbacks
+// Promises
+// async/await
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        return {id: id, userName: 'mosh'}
+
+    }, 2000);
+
+    return 1;
+}
