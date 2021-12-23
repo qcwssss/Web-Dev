92a9070b4d6862705592e636b0353a21d8e4ce65
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-23 11:00:29 -0800

    update promise

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,17 +1,23 @@
 console.log('Before');
-getUser(1, getRepositories);
+// getUser(1, getRepositories);
 
-function getRepositories(user) {
-    getRepositories(user.username, getCommits);
-}
+// function getRepositories(user) {
+//     getRepositories(user.username, getCommits);
+// }
 
-function getCommits(repos) {
-    getCommits(repo, dispalyCommits);
-}
+// function getCommits(repos) {
+//     getCommits(repos, dispalyCommits);
+// }
 
-function dispalyCommits(commits) {
-    console.log(commits);
-}
+// function dispalyCommits(commits) {
+//     console.log(commits);
+// }
+
+getUser(1)
+    .then(user => getRepositories(user.username))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
 
 console.log('After');
 
@@ -19,15 +25,30 @@ console.log('After');
 // Promises
 // async/await
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({id: id, username: 'mosh'});
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({id: id, username: 'mosh'});
+        }, 2000);
+    }) 
+
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['repo1', 'repo2', 'repo3'])
+        }, 2000);
+    });
+}
+
+function getCommits(repo) {
+    return new Promise((resovle, reject) => {
+        setTimeout(() => {
+            console.log('Calling Github API...');
+            resovle(['commit', repo]);
+        }, 2000);
+    })
+
 }
