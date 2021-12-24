54cad6cd270f7747274ebb1bc5c8d9f0f092a97e
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-23 19:23:32 -0800

    add logic operator

--- mongodb-demo/index.js
+++ mongodb-demo/index.js
@@ -34,9 +34,26 @@ async function createCourse() {
     console.log(result);
 }
 
+// mongoDB query operator
+// eq (equal)
+// ne (not equal)
+// gt (greater than)
+// gte (greater than or equal to)
+// lt (less than)
+// lte (less than or equal to)
+// in 
+// nin (not in)
+
 async function getCourses() {
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: true })
+        // .find({ author: 'Mosh', isPublished: true })
+        // .find({ price: { $gte: 10, $lte: 20 } })
+        // .find({ price: { $in: [10, 15, 20] } })
+        
+        // or, and
+        .find()
+        .or([ { author: 'Mosh'}, { isPublished: true} ])
+        // .and([ ])
         .limit(10)
         .sort({ name: 1 })
         .select({name: 1, tags: 1})
