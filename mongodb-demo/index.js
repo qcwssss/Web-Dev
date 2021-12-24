63825243fa464e6143db46a26240e4b460f4e516
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-23 17:07:59 -0800

    start connect

---
+++ mongodb-demo/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+.then (() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could no connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now },
+    isPublished: Boolean
+});
