22e57888ab56882e14d8e85b64bf2319d132af24
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-23 19:13:37 -0800

    update query

--- mongodb-demo/index.js
+++ mongodb-demo/index.js
@@ -1,13 +1,47 @@
+const {
+    MongoNetworkError
+} = require('mongodb');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
-.then (() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could no connect to MongoDB...', err));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could no connect to MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [String],
-    date: {type: Date, default: Date.now },
+    date: {
+        type: Date,
+        default: Date.now
+    },
     isPublished: Boolean
 });
+
+// Classes, objects
+// Course, nodeCourse
+const Course = mongoose.model('Course', courseSchema); // class
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({name: 1, tags: 1})
+    console.log(courses);
+}
+
+// createCourse();
+getCourses();
