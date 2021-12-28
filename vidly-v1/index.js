1bd25ebc7816ab879ae21fcbe0be9e2d32995e2a
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-28 12:06:49 -0800

    add JWT auth

--- vidly-v1/index.js
+++ vidly-v1/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const config = require('config');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
@@ -17,6 +24,8 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 
 const port = 3000 || process.env.PORT;
 app.listen(port, ()=> {
--- vidly-v1/routes/users.js
+++ vidly-v1/routes/users.js
@@ -1,4 +1,6 @@
 const {User, validate} = require('../models/User');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
@@ -15,14 +17,12 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered');
     // console.log('User is:' , user);
-    user = new User({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     
     user = await user.save();
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
